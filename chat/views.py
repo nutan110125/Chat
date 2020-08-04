66c5c1a51a4a95e3f135619bf877e222a16e01eb
nutan110125 <nutan110125@gmail.com>
2020-08-04 21:14:29 +0530

    changes

--- chat/views.py
+++ chat/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'chat/index.html', {})
 
-def room(request, room_name):
+def room(request, sender, receiver):
     return render(request, 'chat/room.html', {
         'sender': sender,
         'receiver':receiver
